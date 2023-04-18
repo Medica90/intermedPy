188baa1e7e74b407a621101a687b2de06ceb0f57
maria <smolkina90@yandex.ru>
2023-04-18 15:33:59 +0500

    final1

--- Notepad.py
+++ Notepad.py
@@ -1,64 +1,111 @@
-notes = {}
+import json
+import datetime
 
+filename = 'notes.json'
+
+def load_notes():
+    try:
+        with open(filename, 'r') as f:
+            notes = json.load(f)
+    except FileNotFoundError:
+        notes = []
+    return notes
+
+def save_notes(notes):
+    with open(filename, 'w') as f:
+        json.dump(notes, f)
+
+def get_notes(query=None):
+    notes = load_notes()
+    if query:
+        notes = [note for note in notes if note['date'] == query]
+    if not notes:
+        print("Заметки отсутствуют!")
+    return notes
+
+
+def view_note(note_id):
+    notes = load_notes()
+    note = next((note for note in notes if note['id'] == note_id), None)
+    if note:
+        return note
+    else:
+        return None
 
 def add_note():
-    title = input("Введите название заметки: ")
-    content = input("Введите содержимое заметки: ")
-    notes[title] = content
-    print(f"Заметка {title} успешно добавлена.")
-
+    notes = load_notes()
+    note_title = input("Введите заголовок заметки: ")
+    note_text = input("Введите текст заметки: ")
+    note_date = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+    note_id = len(notes) + 1
+    notes.append({'id': note_id, 'title': note_title, 'text': note_text, 'date': note_date})
+    save_notes(notes)
 
 def edit_note():
-    title = input("Введите название заметки, которую хотите отредактировать: ")
-    content = notes.get(title)
-    if content:
-        new_content = input("Введите новое содержимое заметки: ")
-        notes[title] = new_content
-        print(f"Заметка {title} успешно изменена.")
+    notes = load_notes()
+    note_id = int(input("Введите номер заметки для редактирования: "))
+    note = next((note for note in notes if note['id'] == note_id), None)
+    if note:
+        note_title = input("Введите новый заголовок заметки: ")
+        note_text = input("Введите новый текст заметки: ")
+        note_date = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+        note['title'] = note_title
+        note['text'] = note_text
+        note['date'] = note_date
+        save_notes(notes)
     else:
-        print(f"Заметка {title} не найдена.")
-
+        print("\nЗаметка не найдена!")
 
 def delete_note():
-    title = input("Введите название заметки, которую хотите удалить: ")
-    if title in notes:
-        del notes[title]
-        print(f"Заметка {title} успешно удалена.")
+    notes = load_notes()
+    note_id = int(input("Введите номер заметки для удаления: "))
+    note = next((note for note in notes if note['id'] == note_id), None)
+    if note:
+        notes.remove(note)
+        save_notes(notes)
+        print("\nЗаметка удалена!")
     else:
-        print(f"Заметка {title} не найдена.")
+        print("\nЗаметка не найдена!")
+
+print("\nПриветствую Вас в моей системе заметок.")
+
+while True:
+    print("\nВыберите действие:")
+    print("1 - просмотреть список заметок")
+    print("2 - просмотреть заметку")
+    print("3 - добавить заметку")
+    print("4 - редактировать заметку")
+    print("5 - удалить заметку")
+    print("0 - выход")
+
+    choice = input("\nВаш выбор: ")
+
+    if choice == '1':
+        query = input("Введите дату для выборки (dd-mm-yyyy) или нажмите Enter для вывода всех заметок: " '\n')
+        notes = get_notes(query)
+        for note in notes:
+            print(note['id'], note['title'], note['date'])
 
 
-def print_notes():
-    if notes:
-        print("Список заметок:")
-        for title, content in notes.items():
-            print(f"{title}: {content}")
-    else:
-        print("Список заметок пуст.")
-
-
-def main():
-    while True:
-        print("Меню:")
-        print("1. Добавить заметку")
-        print("2. Редактировать заметку")
-        print("3. Удалить заметку")
-        print("4. Показать все заметки")
-        print("5. Выйти")
-        choice = input("Введите номер действия: ")
-        if choice == "1":
-            add_note()
-        elif choice == "2":
-            edit_note()
-        elif choice == "3":
-            delete_note()
-        elif choice == "4":
-            print_notes()
-        elif choice == "5":
-            break
+    elif choice == '2':
+        note_id = int(input("Введите номер заметки для просмотра: "))
+        note = view_note(note_id)
+        if note:
+            print()
+            print(note['title'], note['date'])
+            print(note['text'])
         else:
-            print("Некорректный ввод. Попробуйте еще раз.")
+            print("\nЗаметка не найдена!")
 
+    elif choice == '3':
+        add_note()
 
-if __name__ == '__main__':
-    main()
+    elif choice == '4':
+        edit_note()
+
+    elif choice == '5':
+        delete_note()
+
+    elif choice == '0':
+        print("\n!")
+        break
